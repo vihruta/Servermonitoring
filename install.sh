9b48cf8afa4ca910391f74d59341bbbef972c41d
Alexander Vikhorov <vihruta@gmail.com>
2026-09-07 17:45:48 +0300

    Made install.sh for smartctl

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if !command -v smartctl >/dev/null 2>&1; then
+    echo "Installing smartmontools..."
+    sudo apt update
+    sudo apt install -y smartmontools
+else
+    echo "smartctl already installed"
+fi
